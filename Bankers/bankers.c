4214a1858f78874c303b1aae1b9090a8b0d4e75e
amrithm98 <amrithchallenger@gmail.com>
2017-08-11 08:16:19 +0530

    Bankers Algo Complete

--- Bankers/bankers.c
+++ Bankers/bankers.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 
+void findNeed(int need[][20],int max[][20],int alloc[][20],int n,int m);
+void findSafeState(int avail[],int max[][20],int alloc[][20],int n,int m);
+
 int main()
 {
-    int max[20][20],alloc[20][20],need[20][20];
-    int m,n,i,j,k;
+    int max[20][20],alloc[20][20],avail[20];
+    int m,n,i,j;
     printf("\nEnter Number of Processes:");
-    cin>>n;
+    scanf("%d",&n);
     printf("\nEnter Number of Resources:");
-    cin>>m;
+    scanf("%d",&m);
     printf("\nEnter The Demand Matrix(%d X %d): ",n,m);
     for(i=0;i<n;i++)
     {
@@ -24,4 +27,74 @@ int main()
             scanf("%d",&alloc[i][j]);
         }
     }
+    printf("\nEnter The Available Resources(%d resources):",m);
+    for(i=0;i<m;i++)
+        scanf("%d",&avail[i]);
+
+    findSafeState(avail,max,alloc,n,m);
+
+}
+
+void findNeed(int need[][20],int max[][20],int alloc[][20],int n,int m)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            need[i][j]=max[i][j]-alloc[i][j];
+        }
+    }
+}
+
+void findSafeState(int avail[],int max[][20],int alloc[][20],int n,int m)
+{
+    int need[20][20];
+    int work[20],finish[20],safeSequence[20];
+    int i,j,k;
+
+    findNeed(need,max,alloc,n,m);
+    
+    for(i=0;i<n;i++)
+        finish[i]=0;
+
+    for(i=0;i<m;i++)
+        work[i]=avail[i];
+    
+    int count=0;
+
+    while(count<n)
+    {
+        int flag=1;
+        for(i=0;i<n;i++)
+        {
+            if(finish[i]==0)
+            {
+                for(j=0;j<m;j++)
+                {
+                    if(need[i][j]>work[j])
+                        break;
+                }
+                if(j==m)
+                {
+                    for(k=0;k<m;k++)
+                    {
+                        work[k]+=alloc[i][k];
+                    }
+                    safeSequence[count]=i;
+                    count+=1;
+                    finish[i]=1;
+                    flag=1;
+                }
+            }
+        }
+        if(!flag)
+        {
+            printf("\nSystem Not in Safe State");
+            return;
+        }
+    }
+    printf("\nSafe Sequence Is: ");
+    for(i=0;i<n;i++)
+        printf("%d\t",safeSequence[i]);    
 }
