53524d834264a4490ee8b67748993cf0019896e1
amrithm98 <amrithchallenger@gmail.com>
2017-08-11 10:17:02 +0530

    Disk Sched Started

---
+++ Disk-Scheduling/disk_scheduling.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int tracks[20],head=0,n,ch;
+    printf("\nDISK SCHEDULING METHODS");
+    printf("\n-------------------------");
+    printf("\nPress 1 To Enter Head Positions");
+    printf("\nPress 2 To Do FCFS Scheduling");
+    printf("\nPress 3 To Do SCAN Scheduling");
+    printf("\nPress 4 To Do C-SCAN Scheduling");
+    printf("\nPress 5 To Exit");
+    switch(ch)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            exit(0);
+            break;
+
+    }
+
+
+}
